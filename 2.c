b10ae0a6d49132026a44b3fdb8e9287e1477ff75
Тоданов Андрей Викторович группа ИП-414 <andrey.todanov@mail.ru>
2024-11-12 14:43:03 +0700

    Fix

--- 2.c
+++ 2.c
@@ -4,22 +4,6 @@
 #define M 5
 #define N 5
 
-void delenie(float A[M][N]) {
-    for (int i = 0; i < M; i++) {
-        float sum = 0.0;
-
-        for (int j = 0; j < N; j++) {
-            sum += A[i][j];
-        }
-
-        if (sum != 0) {
-            for (int j = 0; j < N; j++) {
-                A[i][j] /= sum;
-            }
-        }
-    }
-}
-
 int main() {
     float A[M][N];
 
@@ -33,12 +17,24 @@ int main() {
     }
 
 
-    delenie(A);
+    for (int i = 0; i < M; i++) {
+        float sum = 0.0;
+
+        for (int j = 0; j < N; j++) {
+            sum += A[i][j];
+        }
+
+        if (sum != 0) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] /= sum;
+            }
+        }
+    }
 
     printf("\nМатрица после деления строк:\n");
     for(int i = 0; i < M; i++) {
         for(int j = 0; j < N; j++) {
-            printf(" %.3f", A[i][j]);
+            printf(" %.2f", A[i][j]);
         }
         printf("\n");
     }
